736613c0a2b088e2b95c9695e2efa89cd6a0053d
tuandht97 <tuandht97@gmail.com>
2022-10-31 17:25:31 +0700

    First commit

---
+++ src/tuan/codecademy/Trouser.java
@@ -0,0 +1,14 @@
+package tuan.codecademy;
+
+import java.io.Serializable;
+
+public class Trouser extends Armour implements Serializable {
+
+  /* Class Variables */
+  private static final long serialVersionUID = 1L;
+
+  /* Constructors */
+  public Trouser(String name, int durability, int defenseRating) {
+    super(name, durability, defenseRating);
+  }
+}
